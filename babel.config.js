8302559f05b3b8ce49f01f0c170bebcf380c2a5c
poulou <poulou_0@hotmail.com>
2023-04-15 16:37:53 +0200

    one small step for dev..

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Poulou <pouloutidis.d@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+const babelConfig = require('@nextcloud/babel-config')
+
+module.exports = babelConfig
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Poulou <pouloutidis.d@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+module.exports = webpackConfig
