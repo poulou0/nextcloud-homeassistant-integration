109d0bf12b35e562f5c1996130af4c180870b2b9
poulou0 <poulou.0@gmail.com>
2023-04-15 16:46:36 +0200

    1. Prepare the app skeleton

--- lib/Service/NoteNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Poulou <pouloutidis.d@gmail.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace OCA\HassIntegration\Service;
-
-class NoteNotFound extends \Exception {
-}
