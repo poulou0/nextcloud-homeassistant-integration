701bf3cde894084bb134a21c800116e9d064a98a
poulou0 <poulou.0@gmail.com>
2023-04-18 14:12:21 +0200

    replace deprecated 'OCP.AppConfig.setValue'

--- src/admin-settings.js
+++ src/admin-settings.js
@@ -1,8 +1,12 @@
 import { showSuccess, showError } from '@nextcloud/dialogs'
+import { generateOcsUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios'
+import { confirmPassword } from '@nextcloud/password-confirmation'
 import '@nextcloud/dialogs/dist/index.css'
+import '@nextcloud/password-confirmation/dist/style.css'
 
 document.addEventListener('DOMContentLoaded', function() {
-	const inputHandler = function(evt) { saveSetting(this.id) }
+	const inputHandler = function(evt) { saveSetting(this.id, this.value) }
 	document.querySelector('#base_url').addEventListener('change', inputHandler)
 	document.querySelector('#long_lived_access_token').addEventListener('change', inputHandler)
 	document.querySelector('#hass_template').addEventListener('change', inputHandler)
@@ -12,17 +16,17 @@ document.addEventListener('DOMContentLoaded', function() {
 /**
  * Save a setting by name.
  *
- * @param {string} settingName The name/id of the field.
+ * @param {string} key The name/id of the field.
+ * @param {string} value The value to be set.
  * @return {void}
  */
-function saveSetting(settingName) {
-	const settingValue = document.querySelector(`#${settingName}`).value
-	OCP.AppConfig.setValue('integration_homeassistant', settingName, settingValue, {
-		success() {
-			showSuccess(`Saved '${settingName.replaceAll('_', ' ').replaceAll('hass ', '')}'!`)
-		},
-		error() {
-			showError(`Error while saving '${settingName}'`)
-		},
-	})
+const saveSetting = async (key, value) => {
+	try {
+		await confirmPassword()
+		const url = generateOcsUrl(`/apps/provisioning_api/api/v1/config/apps/integration_homeassistant/${key}`)
+		await axios.post(url, new URLSearchParams({ value }).toString())
+		showSuccess(`Saved '${key}'!`)
+	} catch (er) {
+		showError(`Error while saving '${key}'`)
+	}
 }
--- templates/admin-settings.php
+++ templates/admin-settings.php
@@ -14,7 +14,12 @@ style(Application::APP_ID, 'admin-settings');
 	<div><?php p($l->t('Base url')); ?></div>
 	<input id="base_url" value="<?php p($_['base_url']) ?>" placeholder="https://..., http://..."/>
 
-	<div><?php p($l->t('Long-lived access token')); ?></div>
+	<div>
+		<?php p($l->t('Long-lived access token')); ?>
+		<a target="_blank" rel="noreferrer" href="https://developers.home-assistant.io/docs/auth_api/#long-lived-access-token">
+			<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="m15.07 11.25l-.9.92C13.45 12.89 13 13.5 13 15h-2v-.5c0-1.11.45-2.11 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41a2 2 0 0 0-2-2a2 2 0 0 0-2 2H8a4 4 0 0 1 4-4a4 4 0 0 1 4 4a3.2 3.2 0 0 1-.93 2.25M13 19h-2v-2h2M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10c0-5.53-4.5-10-10-10Z"/></svg>
+		</a>
+	</div>
 	<textarea id="long_lived_access_token"><?php p($_['long_lived_access_token']) ?></textarea>
 
 	<div><?php p($l->t('Widget refresh interval (in seconds)')); ?></div>
@@ -24,7 +29,7 @@ style(Application::APP_ID, 'admin-settings');
 	<textarea id="hass_template"><?php p($_['hass_template']) ?></textarea>
 	<p>Templates are rendered using the Jinja2 template engine with some Home Assistant specific extensions.</p>
 	<ul>
-		<li><a href="https://jinja.palletsprojects.com/en/latest/templates/" target="_blank" rel="noreferrer">Jinja2 template documentation</a></li>
+		<li><a target="_blank" rel="noreferrer" href="https://jinja.palletsprojects.com/en/latest/templates/">Jinja2 template documentation</a></li>
 		<li><a target="_blank" rel="noreferrer" href="https://www.home-assistant.io/docs/configuration/templating/"> Home Assistant template extensions</a></li>
 	</ul>
 </div>
