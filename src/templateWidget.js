9dbb02ef24b8bdb73c512cab513225bf2d70388e
poulou0 <poulou.0@gmail.com>
2024-06-09 22:06:49 +0200

    nl2br for template widget

--- src/templateWidget.js
+++ src/templateWidget.js
@@ -4,13 +4,21 @@ import { generateUrl } from '@nextcloud/router'
 
 document.addEventListener('DOMContentLoaded', () => {
 	OCA.Dashboard.register('hass-template-widget', (el, { widget }) => {
-		el.innerHTML = loadState('integration_homeassistant', 'dashboard-template-widget')
+		el.innerHTML = nl2br(loadState('integration_homeassistant', 'dashboard-template-widget'))
 		el.parentElement.style.overflow = 'auto'
 		const refreshInterval = parseInt(loadState('integration_homeassistant', 'dashboard-template-widget-refresh-interval'))
 		if (refreshInterval > 0) {
 			setInterval(async () => {
-				el.innerHTML = (await axios.post(generateUrl('/apps/integration_homeassistant/template'))).data[0]
+				el.innerHTML = nl2br((await axios.post(generateUrl('/apps/integration_homeassistant/template'))).data[0])
 			}, refreshInterval * 1000)
 		}
 	})
 })
+
+/**
+ * Replace line breaks with <br>
+ * @param {string} s The variable to convert
+ */
+function nl2br(s) {
+	return s.toString().replace(/(?:\r\n|\r|\n)/g, '<br>')
+}
